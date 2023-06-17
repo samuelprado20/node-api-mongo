af17531817848b056f5d7e909012a33ef2c25e14
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-06-17 16:46:53 -0500

    Add show detail endpoint

--- controllers/show.js
+++ controllers/show.js
@@ -5,10 +5,20 @@ const controller = {
     Show.find({}).exec()
       .then(showsList => {
         if (!showsList) return res.status(404).send({message: "No data found"})
-        console.log(showsList)
         return res.status(200).json(showsList)
       })
       .catch(err => res.status(500).send({message: `Error: ${err}`}))
+  },
+  getShow: function (req, res) {
+    let showId = req.params.id
+    if (showId == null) return res.status(404).send({message: "show not found"})
+
+    Show.findById(showId).exec()
+      .then(data => {
+        if (!data) return res.status(404).send({message: "Show not found"})
+        return res.status(200).json(data)
+      })
+      .catch(err => res.status(500).send({message: `Internal error-> ${err}`}))
   }
 }
 
--- routes/show.js
+++ routes/show.js
@@ -4,5 +4,6 @@ const ShowController = require('../controllers/show')
 const router = Router()
 
 router.get('/', ShowController.getShows)
+router.get('/:id?', ShowController.getShow)
 
 module.exports = router
